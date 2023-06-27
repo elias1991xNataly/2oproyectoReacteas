eed5c05c7e9e0ff4072918e47946691b9f10909d
elias1991xNataly <eliasalfonso1991@gmail.com>
2023-06-28 00:05:39 +0200

    css

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ function App() {
   // const firstFiveActor = contacts.slice(0, 5);
 
   const [firstFiveActors, setfirstFiveActors] = useState(contacts.slice(0, 5))
-  //Con esto lo que haces es que estas jugando directamente con el use State, así creo que es más facil despues actualizar
-  // const [contacts, setContacts] = useState(firstFiveActor);
-  // const AddRandomContact = () => {
-  //   const randomPosition = Math.round(Math.random() + contac.length);
-  //   const randomActor = contac[randomPosition];
-  //   contacts.map((contact) => {
-  //     let addContact;
-  //     if (randomActor.id != contact.id) {
-  //       addContact = [...contacts, randomActor]
-  //       setContacts(addContact)
-  //     }
-  //   })
 
-  // }
 
 
 
@@ -36,7 +23,7 @@ function App() {
   const AddAllActors = ActorId => {
     // (randomActor.id != Actor.id)
     const filteredActors = contacts.filter(actor => {
-      return actor.id != ActorId;
+      return actor.id !== ActorId;
 
 
     })
@@ -49,7 +36,7 @@ function App() {
 
   const deleteActor2 = ActorID => {
     const delActor2 = firstFiveActors.filter(actor => {
-      return actor.id != ActorID;
+      return actor.id !== ActorID;
     })
     setfirstFiveActors(delActor2)
   }
@@ -69,10 +56,10 @@ function App() {
 
   const ArraysortedbyPopularity = () => {
     const orderedbyPopularity = firstFiveActors.sort((a, b) => {
-      if (a.popularity  > b.popularity ) {
+      if (a.popularity > b.popularity) {
         return -1;
       }
-      if (a.popularity  < b.popularity) {
+      if (a.popularity < b.popularity) {
         return 1;
 
       } return 0
@@ -89,17 +76,19 @@ function App() {
 
       <div className="responsiveButtons">
 
-        <button onClick={() => AddAllActors(contacts.name)}>Add Random Contact</button>
-        <button onClick={() => ArraysortedbyAlphabeticOrder()}>Sort by A-Z</button>
-        <button onClick={() => ArraysortedbyPopularity()}>Sort by Popularity</button>
+
+        <button class="responsivebtn" onClick={() => AddAllActors(contacts.name)}>Add All Actors</button>
+        <button class="responsivebtn" onClick={() => ArraysortedbyAlphabeticOrder()}>Sort by A-Z</button>
+        <button class="responsivebtn" onClick={() => ArraysortedbyPopularity()}>Sort by Popularity</button>
       </div>
-      <table>
+      <table class="tableStyling">
         <tr class="tableHeader">
           <th> Picture </th>
           <th> Name </th>
           <th> Popularity</th>
           <th> Won an Oscar</th>
           <th> Won an Emmy</th>
+          <th> Delete</th>
         </tr>
         <p>{console.log(firstFiveActors)}</p>
         <tr className="artistsTable">
@@ -110,9 +99,9 @@ function App() {
                 <td> <img src={contact.pictureUrl} class="image" /></td>
                 <td class="actorName"> {contact.name} </td>
                 <td class="actorPopularity"> {(contact.popularity.toFixed(2))} </td>
-                <td class="imageOfOscar">{(contact.wonOscar) ? <img class="imageOfNomination" src="https://static8.depositphotos.com/1050284/850/i/950/depositphotos_8505454-stock-photo-oscar-isolated.jpg" /> : <img class="imageOfNomination" src="https://e7.pngegg.com/pngimages/862/977/png-clipart-unlike-illustration-brazil-meme-estranho-negativo-text-hand.png" />} </td>
-                <td class="imageOfEmmy">{(contact.wonEmmy) ? <img class="imageOfNomination" src="https://cdnb.20m.es/sites/76/2016/09/Por-qu%C3%A9-los-premios-Emmy-se-llaman-as%C3%AD.jpg" /> : <img class="imageOfNomination" src="https://e7.pngegg.com/pngimages/862/977/png-clipart-unlike-illustration-brazil-meme-estranho-negativo-text-hand.png" />} </td>
-                <td><button onClick={() => deleteActor2(contact.id)}> Delete Actor</button></td>
+                <td class="imageOfOscar">{(contact.wonOscar) ? <img class="imageOfNomination" src="https://static8.depositphotos.com/1050284/850/i/950/depositphotos_8505454-stock-photo-oscar-isolated.jpg" /> : ""} </td>
+                <td class="imageOfEmmy">{(contact.wonEmmy) ? <img class="imageOfNomination" src="https://cdnb.20m.es/sites/76/2016/09/Por-qu%C3%A9-los-premios-Emmy-se-llaman-as%C3%AD.jpg" /> : ""} </td>
+                <td><button className="delete-btn" onClick={() => deleteActor2(contact.id)}> Delete Actor</button></td>
               </tr>
 
             )
